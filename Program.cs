b64e138b6500143f8af16c0e746403938af26d33
Josias <87546094+Josyas@users.noreply.github.com>
2021-09-06 20:54:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace conversao_implcita_e_casting {
+    class Program {
+        static void Main(string[] args) {
+
+            float x = 4.5f;
+            double y = x;
+
+            Console.WriteLine(y);
+            //Conversão implicíta
+
+            Console.WriteLine("=============================");
+            double a;
+            float b;
+
+            a = 5.1;
+            b = (float)a;
+
+            Console.WriteLine(b);
+            //Cast
+
+            Console.WriteLine("===============================");   
+
+            double c;
+            int e;
+
+            c = 5.1;
+            e = (int)a;
+            Console.WriteLine(e);
+            //Cast
+
+            Console.WriteLine("=================================");
+
+
+
+
+        }
+    }
+}
